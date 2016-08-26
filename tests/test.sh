af0c4651931814f897f2e19cc9e96a9db648625c
Dmitry Gavriloff <info@imega.ru>
2016-08-26 21:19:41 +0400

    issue #8 Добавил тест на получение задания (#9)

--- tests/test.sh
+++ tests/test.sh
@@ -35,7 +35,19 @@ testAuth() {
     assertTrue "$EXPECTED" "$ACTUAL" "$FUNCNAME body"
 }
 
+testTask() {
+    ACTUAL=$(curl --write-out %{http_code} --silent --output /dev/null "http://$URL/task?t=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdXRoX3Byb3ZpZGVyIjoidmsiLCJ1c2VyX2lkIjo2Njc0OCwidHRsIjo0MzIwMCwia3Zlc3RfaWQiOjEsInBvaW50X2lkIjowfQ.V0d2cNMNMretk_QMR5oa7fYHorrV2MhHTBwA8DsJffw")
+
+    assertTrue 200 $ACTUAL "$FUNCNAME Code"
+
+    EXPECTED='{"description":"\u0412\u044b \u0434\u043e\u043b\u0436\u043d\u044b \u043f\u0440\u0438\u0439\u0442\u0438 \u0441\u044e\u0434\u0430 \u0447\u0442\u043e\u0431\u044b \u043d\u0430\u0447\u0430\u0442\u044c","point_id":0,"total_points":4,"links":{"checkpoint":"http:\/\/kubikvest.xyz\/checkpoint?t=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdXRoX3Byb3ZpZGVyIjoidmsiLCJ1c2VyX2lkIjo2Njc0OCwidHRsIjo0MzIwMCwia3Zlc3RfaWQiOjEsInBvaW50X2lkIjowfQ.V0d2cNMNMretk_QMR5oa7fYHorrV2MhHTBwA8DsJffw"}}'
+    ACTUAL=$(curl --silent "http://$URL/task?t=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdXRoX3Byb3ZpZGVyIjoidmsiLCJ1c2VyX2lkIjo2Njc0OCwidHRsIjo0MzIwMCwia3Zlc3RfaWQiOjEsInBvaW50X2lkIjowfQ.V0d2cNMNMretk_QMR5oa7fYHorrV2MhHTBwA8DsJffw")
+
+    assertTrue "$EXPECTED" "$ACTUAL" "$FUNCNAME body"
+}
+
 testAuth
+testTask
 
 printf '%.0s-' {1..80}
 echo
